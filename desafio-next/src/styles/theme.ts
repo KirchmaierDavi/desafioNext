4d4418df7098e6a019c516f464ac6bc515931502
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2023-02-15 12:43:07 -0300

    inicia arquivos novamente (resolvendo problema anterior)

--- desafio-next/src/styles/theme.ts
+++ desafio-next/src/styles/theme.ts
@@ -4,9 +4,9 @@ export const theme = extendTheme({
     styles:{
         global:{
             body:{
-                
-                
-            },
-        },
-    },
-});
+                bg: linearGradient(133.46deg, rgba(0, 166, 251, 0.2) 0%, rgba(5, 25, 35, 0.2) 100%), #051923;,
+                color:'white',
+            }
+        }
+    }
+})
