4268a97ff86c33af8cfeccbc04abad1f9bb9ba42
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2023-02-26 11:40:46 -0300

    faz mais problemas

--- desafio-next/src/pages/index.js
+++ desafio-next/src/pages/index.js
@@ -1,28 +1,26 @@
-import Head from 'next/head'
-import NextImage from 'next/image'
+import Logo from 'public/blueskins-high-resolution-logo-white-on-transparent-background.png'
 
 import { Inter } from '@next/font/google'
-import { Stack, Box } from '@chakra-ui/react'
+import { Stack, Box, Image } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Stack
-      as= "main"
+      as="main"
       h={"150vh"}
-    >
-     <NextImage
-      as="img"
-      src='/../public/blueskins-high-resolution-logo-white-on-transparent-background.png'
-      height={{base: 70, md: 80, lg: 100, xl: 155}}
-      width={{base: 200, md: 400, lg: 600, xl: 800, '2xl': 1000}}
-      justify={"center"}
       align={"center"}
-     >
-
-     </NextImage>
-
+    >
+      <Stack justify={"flex-start"} spacing={30}>
+        <Image
+          as="img"
+          src={Logo}
+          height={{ base: 70, md: 80, lg: 100, xl: 155 }}
+          width={{ base: 200, md: 400, lg: 600, xl: 800, '2xl': 1000 }}
+          alt={"Logo do Blueskins"}
+        />
+      </Stack>
     </Stack>
   )
 }
