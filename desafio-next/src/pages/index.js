218618094daae257dfebe4cdb6c3139ceface269
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2023-02-27 13:45:47 -0300

    inicia tela inicial

--- desafio-next/src/pages/index.js
+++ desafio-next/src/pages/index.js
@@ -1,25 +1,53 @@
 import Logo from 'public/blueskins-high-resolution-logo-white-on-transparent-background.png'
+import NextImage from 'next/image'
 
 import { Inter } from '@next/font/google'
-import { Stack, Box, Image } from '@chakra-ui/react'
+import { HStack, Stack, Box, Image, Text } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Stack
-      as="main"
-      h={"150vh"}
+      as="div"
+      h={"200vh"}
       align={"center"}
+      justify={"flex-start"}
+      spacing={{ base: 32, md: 64, lg: 128, xl: 140, '2xl': 252 }}
+
     >
-      <Stack justify={"flex-start"} spacing={30}>
-        <Image
+      <Stack align={'center'} spacing={30} marginTop={232}>
+        <NextImage
           as="img"
           src={Logo}
-          height={{ base: 70, md: 80, lg: 100, xl: 155 }}
-          width={{ base: 200, md: 400, lg: 600, xl: 800, '2xl': 1000 }}
-          alt={"Logo do Blueskins"}
+          height={155}
+          width={1000}
         />
+        <Box
+          as='section'
+          w={"fit-content"}
+          height={"fit-content"}
+          bg={"#003554"}
+          py={{ base: 8, md: 12, lg: 20 }}
+          px={{ base: 4, md: 32, lg: 60, xl: 120, '2xl': 180 }}
+          borderRadius={"15px"}
+          boxShadow={20}
+        >
+          <Stack as="div">
+            <Text
+              as={"p"}
+              fontFamily={"Wallpoet"}
+              textAlign={"center"}
+              fontSize={{ base: 12, md: 20, lg: 24, xl: 28, '2xl': 32 }}
+            >
+              OS ITENS MAIS PROCURADOS VOCÊ ENCONTRA AQUI!
+            </Text>
+            <Text as={"span"} color={"#00A6FB"} fontFamily={"Wallpoet"} fontSize={{ base: 12, md: 20, lg: 24, xl: 28, '2xl': 32 }} textAlign={"center"}>PRINCIPAIS DA SEMANA!</Text>
+          </Stack>
+          <HStack as={"div"} spacing={56}>
+
+          </HStack>
+        </Box>
       </Stack>
     </Stack>
   )
