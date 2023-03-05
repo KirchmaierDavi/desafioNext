7f24f4139bb1f1f684b6abe3a1f740399f45110f
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2023-03-04 21:58:52 -0300

    inicia container pro login

---
+++ desafio-next/src/pages/login.tsx
@@ -0,0 +1,17 @@
+import NextImage from 'next/image'
+
+import { Inter } from '@next/font/google'
+import { HStack, Stack, Box, Image, Text } from '@chakra-ui/react'
+import { Footer } from '../components/footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Stack as={"main"} height={"100vh"} align={"center"} justify={"center"}>
+        <Stack width={"60%"} height={"fit-content"} spacing={"62px"}>
+
+        </Stack>
+    </Stack>
+  )
+}
