7aad182f2fad46a43720ff91ab34fdf8debbcbd2
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2023-03-06 18:18:22 -0300

    inicia pagina de funcionarios

---
+++ desafio-next/src/pages/funcionarios.tsx
@@ -0,0 +1,17 @@
+import NextImage from 'next/image'
+
+import { Inter } from '@next/font/google'
+import { HStack, Stack, Box, Image, Text, Input, Button } from '@chakra-ui/react'
+import { Footer } from '../components/footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Stack as={"main"} height={"fit-content"} align={"center"} justify={"center"}>
+        <HStack>
+            
+        </HStack>
+    </Stack>
+  )
+}
