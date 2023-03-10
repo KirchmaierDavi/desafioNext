8f5199108b942208ac77a7cc8d9236f8b11e44d2
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2023-03-09 22:00:59 -0300

    arruma problema no grid

--- desafio-next/src/pages/equipe.tsx
+++ desafio-next/src/pages/equipe.tsx
@@ -36,11 +36,11 @@ export default function Home() {
         conheça nossa equipe:
       </Text>
       <Grid templateColumns={{base: "repeat(1, 1fr)",md: "repeat(2, 1fr)" ,lg: "repeat(3, 1fr)", xl: "repeat(5, 1fr)"}} gap={20}>
-        <GridItem w="100%"><Pessoa nome={"Davi Kirchmaier Paiva"} img={"/public/foto.jpeg"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
-        <GridItem w="100%"><Pessoa nome={"Davi"} img={"/public/foto.jpeg"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
-        <GridItem w="100%"><Pessoa nome={"Davi"} img={"/public/foto.jpeg"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
-        <GridItem w="100%"><Pessoa nome={"Davi"} img={"/public/foto.jpeg"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
-        <GridItem w="100%"><Pessoa nome={"Davi"} img={"/public/foto.jpeg"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
+        <GridItem w="100%"><Pessoa nome={"Davi Kirchmaier Paiva"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
+        <GridItem w="100%"><Pessoa nome={"Davi"}  idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
+        <GridItem w="100%"><Pessoa nome={"Davi"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
+        <GridItem w="100%"><Pessoa nome={"Davi"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
+        <GridItem w="100%"><Pessoa nome={"Davi"} idade={"19"} email={"davi.kirchmaier99@gmail.com"} cargo={"assessor"}/></GridItem>
       </Grid>
     </Stack>
   );
